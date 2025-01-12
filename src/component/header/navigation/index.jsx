8cebb033e0ae8080e9246ccd580ae52fcd66610c
HUYHOANG <hoanghuydogh123>
2025-01-12 16:29:49 +0700

    Update

--- src/component/header/navigation/index.jsx
+++ src/component/header/navigation/index.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa6";
@@ -14,21 +14,79 @@ import {
 import { TbBrandBlogger } from "react-icons/tb";
 
 const Navigation = () => {
+  const [isCategoryOpen, setCategoryOpen] = useState(false);
+
+  const toggleCategoryMenu = () => {
+    setCategoryOpen((prev) => !prev);
+  };
+
   return (
     <nav>
       <div className="container">
         <div className="row">
+          {/* All Categories Section */}
           <div className="col-sm-2 navPart1">
-            <Button className="allCatTab">
-              <span className="icon1">
-                <IoIosMenu />
-              </span>
-              <span className="text">All Categories</span>
-              <span className="icon2">
-                <FaAngleDown />
-              </span>
-            </Button>
+            <div className="catWrapper">
+              <Button
+                className="allCatTab align-items-center"
+                onClick={toggleCategoryMenu}
+              >
+                <span className="icon1">
+                  <IoIosMenu />
+                </span>
+                <span className="text">All Categories</span>
+                <span className="icon2">
+                  <FaAngleDown />
+                </span>
+              </Button>
+              <div className={`sideBarMenu ${isCategoryOpen ? "active" : ""}`}>
+                <ul>
+                  <li>
+                    <Link to="/fruits-vegetables">
+                      <Button>Fruits & Vegetables</Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/meats-seafood">
+                      <Button>Meats & Seafood</Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/breakfast-dairy">
+                      <Button>Breakfast & Dairy</Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/beverages">
+                      <Button>Beverages</Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/breads-bakery">
+                      <Button>Breads & Bakery</Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/frozen-foods">
+                      <Button>Frozen Foods</Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/biscuits-snacks">
+                      <Button>Biscuits & Snacks</Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/grocery-staples">
+                      <Button>Grocery & Staples</Button>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
+
+          {/* Navigation Links Section */}
           <div className="col-sm-10 navPart2 d-flex align-items-center">
             <ul className="list list-inline">
               <li className="list-inline-item">
@@ -48,29 +106,95 @@ const Navigation = () => {
                   Meats & Seafood
                   <IoFastFoodOutline />
                 </Link>
+                <div className="subMenu">
+                  <ul>
+                    <li>
+                      <Link to="/meats">Fresh Meats</Link>
+                    </li>
+                    <li>
+                      <Link to="/seafood">Seafood</Link>
+                    </li>
+                    <li>
+                      <Link to="/special-cuts">Special Cuts</Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li className="list-inline-item">
                 <Link to="/">
                   Bakery
                   <MdOutlineBakeryDining />
                 </Link>
+                <div className="subMenu">
+                  <ul>
+                    <li>
+                      <Link to="/breads">Breads</Link>
+                    </li>
+                    <li>
+                      <Link to="/cakes">Cakes</Link>
+                    </li>
+                    <li>
+                      <Link to="/pastries">Pastries</Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li className="list-inline-item">
                 <Link to="/">
                   Beverages
                   <MdOutlineEmojiFoodBeverage />
                 </Link>
+                <div className="subMenu">
+                  <ul>
+                    <li>
+                      <Link to="/hot-drinks">Hot Drinks</Link>
+                    </li>
+                    <li>
+                      <Link to="/cold-drinks">Cold Drinks</Link>
+                    </li>
+                    <li>
+                      <Link to="/alcoholic">Alcoholic Beverages</Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li className="list-inline-item">
                 <Link to="/">
                   Blog
                   <TbBrandBlogger />
                 </Link>
+                <div className="subMenu">
+                  <ul>
+                    <li>
+                      <Link to="/recipes">Recipes</Link>
+                    </li>
+                    <li>
+                      <Link to="/tips">Tips</Link>
+                    </li>
+                    <li>
+                      <Link to="/news">News</Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li className="list-inline-item">
                 <Link to="/">
-                  Contact <MdOutlineContactPhone />
+                  Contact
+                  <MdOutlineContactPhone />
                 </Link>
+                <div className="subMenu">
+                  <ul>
+                    <li>
+                      <Link to="/contact-form">Contact Form</Link>
+                    </li>
+                    <li>
+                      <Link to="/faqs">FAQs</Link>
+                    </li>
+                    <li>
+                      <Link to="/support">Support</Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
             </ul>
           </div>
